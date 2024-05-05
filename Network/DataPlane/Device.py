dee1ef779ae93301932cc532b3cd26a6dcf622cd
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2024-05-05 15:01:39 +0200

    Beacon

--- Network/DataPlane/Device.py
+++ Network/DataPlane/Device.py
@@ -1,8 +1,8 @@
 import threading
-
+from socket import PF_PACKET, SOCK_RAW, socket
 from scapy.compat import bytes_hex
 from scapy.sendrecv import sniff
-
+from time import sleep
 from Enforcement import _Enforcement as Enforcement
 import Hasher
 
@@ -14,18 +14,33 @@ def threaded(fn):
     return wrapper
 
 
+def _send(s, data):
+    s.send(hex(data))
+
+
 class Device:
+    BEACON_HASH = hash("")
+    BEACON_INTERVAL = 15
+    BEACON_STATUS = True
+
     def __init__(self, device_id: int, ext_ports=None, int_ports=None):
+        self.ext_ports = ext_ports
+        self.int_ports = int_ports
+        self.device_id = device_id
+        self.enforcement = Enforcement(ldb="../../DB/" + str(self.device_id) + ".db")
+
         if ext_ports is None:
             ext_ports = []
         if int_ports is None:
             int_ports = []
-        self.enforcement = Enforcement(ldb="../../DB/" + str(device_id) + ".db")
 
-        for ext_port in ext_ports:
+        for ext_port in self.ext_ports:
             self.sniff(prn=self.ext_port_recv, iface=ext_port)
-        for int_port in int_ports:
+        for int_port in self.int_ports:
             self.sniff(prn=self.int_port_recv, iface=int_port)
+
+        # start sending beacon every BEACON_INTERVAL
+        self.beacon()
 
     @threaded
     def sniff(self, prn, iface):
@@ -40,10 +55,23 @@ class Device:
         data = bytes_hex(pkt)
         hash = data[0:Hasher.LENGTH]
         print("\nint packet:")
-        print(pkt)
+        print("hash" + str(hash))
+        print(bytes_hex(pkt))
 
-    def start(self):
-        print("start")
+    @threaded
+    def beacon(self):
+        int_sockets = {}
+        # Create socket for each internal port
+        # Add every entry to dict {<port_name>: <socket>}
+        for port in self.int_ports:
+            int_sockets = int_sockets.update({port: socket(PF_PACKET, SOCK_RAW)})
+            int_sockets[port].bind((port, 0))
+        while self.BEACON_STATUS:
+            for port, s in int_sockets.items():
+                print("sending beacon on port " + str(port))
+                data = str(self.BEACON_HASH) + str(self.device_id) + str(port)
+                _send(s, data)
+            sleep(self.BEACON_INTERVAL)
 
 
 if __name__ == '__main__':
