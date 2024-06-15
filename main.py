2deb2abf146ce54848ff3b594c62a158a4d54e5d
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2024-06-15 14:15:10 +0200

    ldb

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     device_id = Hasher.hasher(gethostname().encode())
     configurator_hash = Hasher.hasher("configurator".encode())
     print(device_id)
-    ldb = LDBSQLite("/opt/mgr/ldb/" + str(int.from_bytes(self.device_id, "big")) + ".db")
+    ldb = LDBSQLite("/opt/mgr/ldb/" + str(int.from_bytes(device_id, "big")) + ".db")
     device = Device(device_id=device_id, configurator_hash=configurator_hash, ldb=ldb, int_ifaces=["ens16"])
