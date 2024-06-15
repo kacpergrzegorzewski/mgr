4ab809838bf635b2e53599842bb54115bb6349cc
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2024-06-15 14:25:34 +0200

    ldb added check_same_thread=False

--- Network/DataPlane/LDB.py
+++ Network/DataPlane/LDB.py
@@ -7,20 +7,17 @@ class LDBSQLite:
         self._init_db(filename)
 
     def _init_db(self, filename):
-        self.db = sqlite3.connect(filename)
-        cursor = self.db.cursor()
-        result = cursor.execute("PRAGMA table_info(ldb)")
+        self.db = sqlite3.connect(filename, check_same_thread=False)
+        self.cursor = self.db.cursor()
+        result = self.cursor.execute("PRAGMA table_info(ldb)")
         if result is None:
-            cursor.execute("CREATE TABLE ldb(hash, outport, endtime)")
+            self.cursor.execute("CREATE TABLE ldb(hash, outport, endtime)")
 
     def get_outport(self, hash):
-        cursor = self.db.cursor()
-        return cursor.execute("SELECT outport FROM ldb WHERE hash=?", (hash,))
+        return self.cursor.execute("SELECT outport FROM ldb WHERE hash=?", (hash,))
 
     def put(self, hash, outport, endtime="2099-01-01 12:00:00"):
-        cursor = self.db.cursor()
         cursor.execute("INSERT INTO ldb(hash,outport,endtime) VALUES(?,?,?)", (hash, outport, endtime))
-
 
 if __name__ == '__main__':
     filename = "../../DB/LDB-test01.db"
@@ -31,7 +28,7 @@ if __name__ == '__main__':
     hash = "abc"
     outport = "2"
     endtime = "15:24"
-    # cursor.execute("INSERT INTO ldb(hash,outport,endtime) VALUES(?,?,?)", (hash, outport, endtime))
-    # db.commit()
+    #cursor.execute("INSERT INTO ldb(hash,outport,endtime) VALUES(?,?,?)", (hash, outport, endtime))
+    #db.commit()
     response = cursor.execute("SELECT outport FROM LDB WHERE hash=?", (hash,))
     print(response.fetchone())
