109f2ce1bb2ed0049564fcdc3d22a9867f10bed4
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2024-06-17 17:12:44 +0200

    delete __main__

--- Network/DataPlane/Device.py
+++ Network/DataPlane/Device.py
@@ -116,9 +116,3 @@ class Device:
                 # wait BEACON_INTERVAL before sending next beacon
                 sleep(self.BEACON_INTERVAL)
 
-
-if __name__ == '__main__':
-    device_id = Hasher.hasher(gethostname().encode())
-    configurator_hash = Hasher.hasher("configurator".encode())
-    print(device_id)
-    device = Device(device_id=device_id, configurator_hash=configurator_hash, int_ifaces=["ens16"])
--- Network/DataPlane/LDB.py
+++ Network/DataPlane/LDB.py
@@ -20,16 +20,3 @@ class LDBSQLite:
         self.cursor.execute("INSERT INTO ldb(hash,outport,endtime) VALUES(?,?,?)", (hash, outport, endtime))
         self.db.commit()
 
-if __name__ == '__main__':
-    filename = "../../DB/LDB-test01.db"
-    db = sqlite3.connect(filename)
-    cursor = db.cursor()
-    # cursor.execute("CREATE TABLE ldb(hash, outport, endtime)")
-
-    hash = "abc"
-    outport = "2"
-    endtime = "15:24"
-    #cursor.execute("INSERT INTO ldb(hash,outport,endtime) VALUES(?,?,?)", (hash, outport, endtime))
-    #db.commit()
-    response = cursor.execute("SELECT outport FROM LDB WHERE hash=?", (hash,))
-    print(response.fetchone())
