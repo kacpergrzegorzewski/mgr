3fa68acd1723d4f66b6f9844b7057acc8539c828
Kacper Grzegorzewski <kacper.grzegorzewski@wp.pl>
2024-07-16 16:12:49 +0000

    add path to configurator in main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from Network.DataPlane import Hasher
 from socket import gethostname
 from Network.DataPlane.Device import Device
 from Network.DataPlane.LDB import LDBSQLite
-
+from Network.Base.Env import *
 
 def start_device():
     device_hash = Hasher.hasher(gethostname().encode())
     configurator_hash = Hasher.hasher("configurator".encode())
     print(device_hash)
     ldb = LDBSQLite("/opt/mgr/ldb/" + str(int.from_bytes(device_hash, "big")) + ".db")
-    device = Device(device_hash=device_hash, configurator_hash=configurator_hash, ldb=ldb, int_ifaces=["ens16"])
+    device = Device(device_hash=device_hash, configurator_hash=configurator_hash, ldb=ldb, int_ifaces=["ens16", "ens28", "ens17", "ens27"])
 
 def ldb_test():
     hash = b'\xd4\x1d\x8c\xd9\x8f\x00\xb2\x04\xe9\x80\t\x98\xec\xf8B~'
@@ -19,6 +19,12 @@ def ldb_test():
 #    print(ldb.get_all())
     ldb.put(hash, "ens16")
 
+def add_configurator_path(iface):
+    device_hash = Hasher.hasher(gethostname().encode())
+    ldb = LDBSQLite("/opt/mgr/ldb/" + str(int.from_bytes(device_hash, "big")) + ".db")
+    ldb.put(CONFIGURATOR_HASH, iface)
+    print(ldb.get_outport(CONFIGURATOR_HASH))
 
 if __name__ == '__main__':
     start_device()
+#    add_configurator_path("ens27")
