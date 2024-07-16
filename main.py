e99852c9e442c2f193f9989aa29645d12b51ae35
Kacper Grzegorzewski <kacper.grzegorzewski@wp.pl>
2024-07-16 15:47:49 +0000

    core01

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def start_device():
     configurator_hash = Hasher.hasher("configurator".encode())
     print(device_id)
     ldb = LDBSQLite("/opt/mgr/ldb/" + str(int.from_bytes(device_id, "big")) + ".db")
-    device = Device(device_id=device_id, configurator_hash=configurator_hash, ldb=ldb, int_ifaces=["ens16"])
+    device = Device(device_hash=device_id, configurator_hash=configurator_hash, ldb=ldb, int_ifaces=["ens16", "ens17", "ens27", "ens28"])
 
 def ldb_test():
     hash = b'\xd4\x1d\x8c\xd9\x8f\x00\xb2\x04\xe9\x80\t\x98\xec\xf8B~'
@@ -21,4 +21,4 @@ def ldb_test():
 
 
 if __name__ == '__main__':
-    ldb_test()
+    start_device()
