91315b4839ad93ed1946eada05279db255b13226
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2024-07-18 13:31:11 +0200

    Export TDB to a separate file

--- Network/ControlPlane/Configurator.py
+++ Network/ControlPlane/Configurator.py
@@ -6,6 +6,7 @@ import networkx as nx
 
 from ..Base.Env import *
 from ..Base.InternalPacket import InternalPacket
+from TDB import TDB
 
 
 def threaded(fn):
@@ -22,8 +23,7 @@ class Configurator:
     def __init__(self, iface):
         print("[INFO] Initializing Configurator")
         self.iface = iface
-        self.tdb = nx.DiGraph()
-        self.print_current_state()
+        self.tdb = TDB()
         self.sniff(self.recv, self.iface)
 
     @threaded
@@ -41,39 +41,13 @@ class Configurator:
         internal_packet = InternalPacket(pkt)
         if internal_packet.hash == CONFIGURATOR_LINK_DISCOVERY_HASH:
             print("Received packet: " + str(internal_packet.raw_pkt))
-            self.add_node(internal_packet.link_discovery_src_hash)
-            self.add_node(internal_packet.link_discovery_dst_hash)
-            self.add_edge(
+            self.tdb.add_node(internal_packet.link_discovery_src_hash)
+            self.tdb.add_node(internal_packet.link_discovery_dst_hash)
+            self.tdb.add_edge(
                 start=internal_packet.link_discovery_src_hash,
                 end=internal_packet.link_discovery_dst_hash,
                 src_iface=internal_packet.link_discovery_src_iface,
                 dst_iface=internal_packet.link_discovery_dst_iface
             )
 
-    def add_node(self, node):
-        if node not in self.tdb.nodes:
-            self.tdb.add_node(node)
-            print("[INFO] Added node " + str(node) + " to TDB.")
-        else:
-            print("[INFO] Node " + str(node) + " exists.")
-
-    def add_edge(self, start, end, src_iface, dst_iface):
-        if start and end in self.tdb.nodes:
-            self.tdb.add_edge(start, end, src_iface=src_iface, dst_iface=dst_iface)
-        else:
-            print("[WARNING] Node does not exist. Link " + str(start) + " -> " + str(end) + " not created in TDB.")
-
-    @threaded
-    def print_current_state(self):
-        while self.TDB_PRINT:
-            print("===============================================================")
-            print("Current TDB state:")
-            for edge in self.tdb.edges:
-                src_node = str(edge[0])
-                dst_node = str(edge[1])
-                src_iface = self.tdb.edges[src_node, dst_node]["src_iface"]
-                dst_iface = self.tdb.edges[src_node, dst_node]["dst_iface"]
-                print(src_node + " (" + src_iface + ")-> " + dst_node + "(" + dst_iface + ")")
-            print("===============================================================")
-            time.sleep(self.TDB_PRINT_INTERVAL)
 
