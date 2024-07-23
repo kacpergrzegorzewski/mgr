c63dcea41e5d86df832944b9c425a824f41cd253
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2024-07-23 20:25:07 +0200

    add lock in LDB

--- Network/DataPlane/LDB.py
+++ Network/DataPlane/LDB.py
@@ -1,5 +1,6 @@
 import sqlite3
 import threading
+from threading import Lock
 import time
 
 
@@ -8,6 +9,9 @@ def threaded(fn):
         threading.Thread(target=fn, args=args, kwargs=kwargs).start()
 
     return wrapper
+
+
+lock = Lock()
 
 
 class LDBSQLite:
@@ -21,28 +25,41 @@ class LDBSQLite:
     def _init_db(self, filename):
         self.db = sqlite3.connect(filename, check_same_thread=False)
         self.cursor = self.db.cursor()
+        lock.acquire(True)
         result = self.cursor.execute("PRAGMA table_info(ldb)")
+        lock.release()
         if result.fetchone() is None:
+            lock.acquire(True)
             self.cursor.execute("CREATE TABLE ldb(hash BLOB PRIMARY KEY ON CONFLICT REPLACE, outport, endtime)")
+            lock.release()
         self._delete_old_flows()
 
     def get_outport(self, hash):
+        lock.acquire(True)
         response = self.cursor.execute("SELECT outport FROM ldb WHERE hash=?", (hash,)).fetchone()
+        lock.release()
         if response is None:
             return None
         else:
             return response[0]
 
     def get_all(self):
-        return self.cursor.execute("SELECT * FROM ldb").fetchall()
+        lock.acquire(True)
+        response = self.cursor.execute("SELECT * FROM ldb").fetchall()
+        lock.release()
+        return response
 
     def add_flow(self, hash, outport, endtime="4070908800"):
+        lock.acquire(True)
         self.cursor.execute("INSERT OR REPLACE INTO ldb(hash,outport,endtime) VALUES (?,?,?)",
                             (memoryview(hash), outport, endtime))
         self.db.commit()
+        lock.release()
 
     @threaded
     def _delete_old_flows(self):
         while self.DELETE_OLD_FLOWS:
+            lock.acquire(True)
             self.cursor.execute("DELETE FROM LDB WHERE endtime<?", (str(int(time.time()))))
+            lock.release()
             time.sleep(self.DELETE_OLD_FLOWS_INTERVAL)
