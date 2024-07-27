9f84a63a1afbd0c960edf444c246762983886b3d
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2024-07-27 15:08:43 +0200

    ExternalPacket.py

---
+++ Network/Base/ExternalPacket.py
@@ -0,0 +1,52 @@
+from scapy.layers.l2 import Ether
+from scapy.packet import raw
+import scapy.layers.all
+
+class ExternalPacket:
+    def __init__(self, pkt: Ether):
+        self.pkt = pkt
+        self.iface = pkt.sniffed_on
+        self.raw_pkt = raw(pkt)
+        self.json = self.pkt.json()
+        self.layers = self.get_layers()
+
+        if 'Ethernet' in self.layers:
+            self.mac_src = self.pkt["Ether"].src
+            self.mac_dst = self.pkt["Ether"].dst
+        else:
+            self.mac_src = ""
+            self.mac_dst = ""
+
+        if "IP" in self.layers:
+            self.ip_src = self.pkt["IP"].src
+            self.ip_dst = self.pkt["IP"].dst
+        else:
+            self.ip_src = ""
+            self.ip_dst = ""
+
+        if "TCP" in self.layers:
+            self.src_port = self.pkt["TCP"].sport
+            self.dst_port = self.pkt["TCP"].dport
+        else:
+            self.src_port = ""
+            self.dst_port = ""
+
+        self.to_hash = b''
+        for layer_name in self.layers:
+            self.to_hash += layer_name.encode()
+        self.to_hash += self.mac_src.encode()
+        self.to_hash += self.mac_dst.encode()
+        self.to_hash += self.ip_src.encode()
+        self.to_hash += self.ip_dst.encode()
+        self.to_hash += self.dst_port.encode()
+
+    def get_layers(self):
+        counter = 0
+        layers = []
+        while True:
+            layer = self.pkt.getlayer(counter)
+            if layer is None or layer.name == "Padding":
+                break
+            layers.append(layer.name)
+            counter += 1
+        return layers
--- Network/DataPlane/Device.py
+++ Network/DataPlane/Device.py
@@ -1,6 +1,8 @@
 import threading
 import time
 from socket import PF_PACKET, SOCK_RAW, socket, gethostname
+
+import scapy.packet
 from scapy.compat import bytes_hex
 from scapy.sendrecv import sniff
 from scapy import packet
@@ -9,6 +11,7 @@ from time import sleep
 from .Enforcement import _Enforcement as Enforcement
 from . import Hasher
 from ..Base.InternalPacket import InternalPacket
+from ..Base.ExternalPacket import ExternalPacket
 from ..Base.Env import *
 from collections import deque
 
@@ -108,16 +111,17 @@ class Device:
         :param iface: name of interface
         :return: None
         """
-        sniff(prn=prn, iface=iface)
+        sniff(prn=prn, iface=iface, store=0)
 
     def ext_iface_recv(self, pkt):
         """
         Function triggered for every packet received on external iface
         :param pkt: received packet
         """
-        raw = inet.raw(pkt)
-        print(type(raw))
-        print(raw)
+        pkt = ExternalPacket(pkt)
+        # check if packet is not in last sent packets (sniff also captures sent packets)
+        if self.lastPacket[pkt.iface].count(pkt.raw_pkt) == 0:
+            print("[WARNING] Received external packet with values: " + str(pkt.to_hash))
 
     def int_iface_recv(self, pkt):
         """
@@ -125,6 +129,7 @@ class Device:
         :param pkt: received packet
         """
         pkt = InternalPacket(pkt)
+        # check if packet is not in last sent packets (sniff also captures sent packets)
         if self.lastPacket[pkt.iface].count(pkt.raw_pkt) == 0:
             if pkt.hash == BEACON_HASH:
                 beacon_hash, beacon_iface = pkt.extract_beacon_data()
