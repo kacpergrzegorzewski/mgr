1a7fc4c8d7802c8f11efdef74bd87628b6e0abd8
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2024-07-30 12:39:06 +0200

    remove old links fix

--- Network/ControlPlane/TDB.py
+++ Network/ControlPlane/TDB.py
@@ -89,6 +89,6 @@ class TDB:
         while self.REMOVE_OLD_LINKS:
             current_time = time.time()
             for edge in self.tdb.edges:
-                if current_time > edge["endtime"]:
+                if current_time > self.tdb.get_edge_data(*edge)["endtime"]:
                     self.tdb.remove_edge(*edge)
             time.sleep(self.REMOVE_OLD_LINKS_INTERVAL)
