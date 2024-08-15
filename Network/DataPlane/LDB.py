41c2fe64c2cb78642d616d8df8ac559e8c06d001
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2024-08-15 17:42:34 +0200

    LDBCache

--- Network/DataPlane/LDB.py
+++ Network/DataPlane/LDB.py
@@ -11,19 +11,53 @@ def threaded(fn):
     return wrapper
 
 
+class FixSizedDict(dict):
+    def __init__(self, *args, maxlen=0, **kwargs):
+        self._maxlen = maxlen
+        super().__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, key, value)
+        if self._maxlen > 0:
+            if len(self) > self._maxlen:
+                self.pop(next(iter(self)))
+
+
+class LDBCache:
+    def __init__(self, maxlen=0):
+        self.queue = FixSizedDict(maxlen=maxlen)
+
+    def add(self, flow, outport):
+        self.queue[flow] = outport
+
+    def remove(self, flow):
+        self.queue.pop(flow)
+
+    def remove_many(self, flows):
+        for flow in flows:
+            self.remove(flow)
+
+    def is_hit(self, flow):
+        return flow in self.queue
+
+    def get_outport(self, flow):
+        return self.queue[flow]
+
+
 class LDBSQLite:
     DELETE_OLD_FLOWS = True
     DELETE_OLD_FLOWS_INTERVAL = 0.5
     PRINT_LDB = True
     PRINT_LDB_INTERVAL = 10
 
-    def __init__(self, filename):
+    def __init__(self, filename, cache_size=0):
         print("[INFO] Initializing LDB in " + filename)
         self.number_of_lookups = 0
         self.sum_of_lookup_time = 0
         self.number_of_writes = 0
         self.sum_of_write_time = 0
         self.db_lock = Lock()
+        self.cache = LDBCache(maxlen=cache_size)
         self._init_db(filename)
         self._delete_old_flows()
         self._print_ldb()
@@ -40,17 +74,21 @@ class LDBSQLite:
             self.db_lock.release()
 
     def get_outport(self, hash):
+        if self.cache.is_hit(hash):
+            return self.cache.get_outport(hash)
         self.number_of_lookups += 1
         time_before = time.time_ns()
         self.db_lock.acquire(True)
         response = self.cursor.execute("SELECT outport FROM ldb WHERE hash=?", (hash,)).fetchone()
         self.db_lock.release()
         time_after = time.time_ns()
-        self.sum_of_lookup_time += (time_after - time_before)/1_000_000  # time in ms
+        self.sum_of_lookup_time += (time_after - time_before) / 1_000_000  # time in ms
         if response is None:
             return None
         else:
-            return response[0]
+            outport = response[0]
+            self.cache.add(hash, outport)
+            return outport
 
     def get_all(self):
         self.db_lock.acquire(True)
@@ -60,6 +98,7 @@ class LDBSQLite:
 
     @threaded
     def add_flow(self, hash, outport, endtime="4070908800"):  # 01.01.2099
+        self.cache.add(hash, outport)
         time_before = time.time_ns()
         self.db_lock.acquire(True)
         self.cursor.execute("INSERT OR REPLACE INTO ldb(hash,outport,endtime) VALUES (?,?,?)",
@@ -67,18 +106,21 @@ class LDBSQLite:
         self.db.commit()
         self.db_lock.release()
         time_after = time.time_ns()
-        self.sum_of_write_time += (time_after-time_before) / 1_000_000  # ms
+        self.sum_of_write_time += (time_after - time_before) / 1_000_000  # ms
         self.number_of_writes += 1
 
     @threaded
     def _delete_old_flows(self):
         while self.DELETE_OLD_FLOWS:
             current_time = str(int(time.time()))
-            query = "DELETE FROM LDB WHERE endtime<" + current_time
+            query_delete = "DELETE FROM LDB WHERE endtime<" + current_time
+            query_select = "SELECT hash FROM LDB WHERE endtime<" + current_time
             self.db_lock.acquire(True)
-            self.cursor.execute(query)
+            self.cursor.execute(query_delete)
+            to_remove = self.cursor.execute(query_select)
             self.db.commit()
             self.db_lock.release()
+            self.cache.remove_many(to_remove)
             time.sleep(self.DELETE_OLD_FLOWS_INTERVAL)
 
     @threaded
