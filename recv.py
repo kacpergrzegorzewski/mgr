16d1c837ca1509c262b13f2bb3d37441eb2568a0
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2025-06-28 13:35:51 +0200

    bandwidth tester

--- recv.py
+++ recv.py
@@ -1,5 +1,19 @@
+import threading
+import time
+from time import sleep
+
 from scapy.all import sniff, bytes_hex
 
+IFACE = 'ens16'
+PACKET_COUNTER = 0
+PRINT_PACKET_COUNTER = True
+BANDWIDTH_TEST_MESSAGE_LENGTH = 1500
+
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        threading.Thread(target=fn, args=args, kwargs=kwargs).start()
+
+    return wrapper
 
 def mod(pkt):
     print((bytes_hex(pkt)).decode("utf-8"))
@@ -9,5 +23,29 @@ def edge(pkt):
     print(pkt)
     print(pkt)
 
+def increment_packet_counter():
+    global PACKET_COUNTER
+    PACKET_COUNTER += 1
 
-sniff(prn=mod, iface="ens16")
+@threaded
+def count_packets():
+    global PACKET_COUNTER
+    global PRINT_PACKET_COUNTER
+
+    while PRINT_PACKET_COUNTER:
+        packet_counter_before = PACKET_COUNTER
+        time_before = time.time()
+        sleep(1)
+        packet_counter_after = PACKET_COUNTER
+        time_after = time.time()
+        time_passed = time_after - time_before
+        packets_passed = packet_counter_after - packet_counter_before
+        mbs = packets_passed * BANDWIDTH_TEST_MESSAGE_LENGTH / time_passed / 1024
+        print("Received " + str(packets_passed) + " in " + str(time_passed) + " (" + str(mbs) + " MB/s)")
+
+@threaded
+def sniff_threaded():
+    sniff(prn=increment_packet_counter, iface=IFACE)
+
+if __name__ == '__main__':
+    sniff_threaded()
--- send.py
+++ send.py
@@ -2,12 +2,32 @@ from socket import socket, AF_PACKET, PF_PACKET, SOCK_RAW, IPPROTO_RAW
 import struct
 import time
 
+BANDWIDTH_TEST_MESSAGE_LENGTH = 1500 #bytes
+
 _socket = socket(PF_PACKET, SOCK_RAW)
 _socket.bind(('ens28', 0))
 
-data = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.").encode()
-print(data.hex())
-_socket.send(data)
+def send_simple_message():
+    data = (
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.").encode()
+    print(data.hex())
+    _socket.send(data)
+
+def generate_bandwidth_test_message():
+    current_message_length = 1
+    even = False
+    message = 1
+    while current_message_length < 8 * BANDWIDTH_TEST_MESSAGE_LENGTH:
+        message = message << 1
+        if even:
+            message += 1
+        current_message_length += 1
+        even = not even
+    return message.to_bytes(length=BANDWIDTH_TEST_MESSAGE_LENGTH, byteorder='big')
+
+def send_bandwidth_test(message):
+    while True:
+        _socket.send(message)
 
 def send(data, iface):
     '''
@@ -19,8 +39,6 @@ def send(data, iface):
     _socket.bind((iface, 0))
     _socket.send(data)
 
-#for i in range(50):
-#	data = ("to jest " + str(i) + " wiadomosc.").encode()
-#	print(data)
-#	socket.send(data)
-#	time.sleep(0.1)
+
+if __name__ == '__main__':
+    print(generate_bandwidth_test_message())
